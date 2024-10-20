e87c17d205a078c751d1a0b27634434b68dca3ee
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-10-20 12:47:30 +0500

    some files

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
 
 const posts = [
    { title: 'React Testing', excerpt: 'Learn React Testing' },
@@ -14,16 +15,12 @@ export default function Home() {
          </Head>
          <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
             <div className="col-span-1 lg:col-span-8">
-               {posts.map((post, index) => (
-                  <div>
-                     {post.title}
-                     {post.excerpt}
-                  </div>      
-               ))}
+               {posts.map((post) => <PostCard post={post} key={post.title} />)}
             </div>
             <div className="col-span-1 lg:col-span-4">
                <div className="relative lg:sticky top-8">
-               
+                  <PostWidget />
+                  <Categories />
                </div>
             </div>
          </div>
