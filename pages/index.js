deb39a42be29ac9267799119b340015c3177b787
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-10-21 11:52:02 +0500

    fatching data using gql, added env file

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
 import Head from "next/head";
-import { PostCard, Categories, PostWidget } from "../components";
+import { PostCard, Categories, PostWidget, Header } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-   { title: 'React Testing', excerpt: 'Learn React Testing' },
-   { title: 'React with Tailwind', excerpt: 'Learn React with tailwind' },
-]
 
-export default function Home() {
+export default function Home({ posts }) {
    return (
-      <div className="container px-10 mx-auto mb-8 bg-gray-300">
+      <div className="container px-10 mx-auto mb-8">
          <Head>
             <title>CMS Blog</title>
             <link rel="icon" href="/favicon.ico" />
          </Head>
          <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
             <div className="col-span-1 lg:col-span-8">
-               {posts.map((post) => <PostCard post={post} key={post.title} />)}
+               {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
             </div>
             <div className="col-span-1 lg:col-span-4">
                <div className="relative lg:sticky top-8">
@@ -26,4 +23,12 @@ export default function Home() {
          </div>
       </div>
    )
+}
+
+export async function getStaticProps() {
+   const posts = (await getPosts()) || [];
+
+   return {
+      props: { posts }
+   }
 }
