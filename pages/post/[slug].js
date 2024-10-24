973f30dce270080413a01ffdd6f7f22228f639ba
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-10-24 10:35:49 +0500

    added query for post details

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,19 +3,19 @@ import React from 'react';
 import { getPosts, getPostDetails } from '../../services';
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components';
 
-const postDetails = () => {
+const postDetails = ({ post }) => {
   return (
     <div className='container px-10 mx-auto mb-8'>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
          <div className="col-span-1 lg:col-span-8">
-            <PostDetail />
-            <Author />
-            <CommentsForm />
-            <Comments />
+            <PostDetail post={post} />
+            <Author author={post.author} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
          </div>
          <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">
-               <PostWidget />
+               <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
                <Categories />
             </div>
          </div>
@@ -27,9 +27,18 @@ const postDetails = () => {
 export default postDetails;
 
 export async function getStaticProps({ params }) {
-   const posts = (await getPosts()) || [];
+   const data = await getPostDetails(params.slug);
 
    return {
-      props: { posts }
+      props: { posts: data }
+   }
+}
+
+export async function getStaticPaths() {
+   const posts = await getPosts();
+   
+   return {
+      paths: posts.map(({ node: { slug }}) => ({ params: { slug }})),
+      fallback: false,
    }
 }
--- services/index.js
+++ services/index.js
@@ -39,6 +39,41 @@ export const getPosts = async () => {
    return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+   const query = gql`
+      query GetPostDetails($slug: String!) {
+         post(where: { slug: $slug }) {
+            author {
+               bio
+               name
+               id
+               photo {
+                  url
+               }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+               url
+            }
+            category {
+               name
+               slug
+            }
+            content {
+               raw
+            }
+         }
+      }
+   `
+
+   const result = await request(graphqlAPI, query, { slug });
+
+   return result.post;
+};
+
 export const getRecentPosts = async () => {
    const query = gql`
       query GetPostDetails {
@@ -78,7 +113,7 @@ export const getSimilarPosts = async (categories, slug) => {
       }
    `;
 
-   const result = await request(graphqlAPI, query, { slug, categories });
+   const result = await request(graphqlAPI, query, { categories, slug });
 
    return result.posts;
 }
